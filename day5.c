e0c2ee00752b4594d7ee2e2ee33ae015857f3a26
gharatsamyaak-dotcom <gharatsamyaak@gmail.com>
2026-02-05 23:10:37 +0530

    day5b

--- day5.c
+++ day5.c
@@ -1,26 +1,40 @@
-p = int(input())
-log1 = list(map(int, input().split()))
+#include <stdio.h>
 
-q = int(input())
-log2 = list(map(int, input().split()))
+int main() {
+    int p, q;
+    int i = 0, j = 0;
 
-i = j = 0
-merged_log = []
+    scanf("%d", &p);
+    int log1[p];
+    for (int k = 0; k < p; k++) {
+        scanf("%d", &log1[k]);
+    }
 
-while i < p and j < q:
-    if log1[i] <= log2[j]:
-        merged_log.append(log1[i])
-        i += 1
-    else:
-        merged_log.append(log2[j])
-        j += 1
+    scanf("%d", &q);
+    int log2[q];
+    for (int k = 0; k < q; k++) {
+        scanf("%d", &log2[k]);
+    }
 
-while i < p:
-    merged_log.append(log1[i])
-    i += 1
+    while (i < p && j < q) {
+        if (log1[i] <= log2[j]) {
+            printf("%d ", log1[i]);
+            i++;
+        } else {
+            printf("%d ", log2[j]);
+            j++;
+        }
+    }
 
-while j < q:
-    merged_log.append(log2[j])
-    j += 1
+    while (i < p) {
+        printf("%d ", log1[i]);
+        i++;
+    }
 
-print(*merged_log)
+    while (j < q) {
+        printf("%d ", log2[j]);
+        j++;
+    }
+
+    return 0;
+}
