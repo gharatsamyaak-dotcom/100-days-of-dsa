ddd143e5412989836127a573b8c7a04f18776853
gharatsamyaak-dotcom <gharatsamyaak@gmail.com>
2026-03-31 11:31:00 +0530

    Day 59 - DSA Challenge

---
+++ day59.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Search in inorder
+int search(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+// Build tree
+struct Node* buildTree(int inorder[], int postorder[], int start, int end, int* postIndex) {
+    if (start > end)
+        return NULL;
+
+    // Pick root from postorder
+    int curr = postorder[(*postIndex)--];
+    struct Node* root = newNode(curr);
+
+    if (start == end)
+        return root;
+
+    int inIndex = search(inorder, start, end, curr);
+
+    // IMPORTANT: build right first
+    root->right = buildTree(inorder, postorder, inIndex + 1, end, postIndex);
+    root->left  = buildTree(inorder, postorder, start, inIndex - 1, postIndex);
+
+    return root;
+}
+
+// Preorder traversal
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int inorder[n], postorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &postorder[i]);
+
+    int postIndex = n - 1;
+
+    struct Node* root = buildTree(inorder, postorder, 0, n - 1, &postIndex);
+
+    preorder(root);
+
+    return 0;
+}
